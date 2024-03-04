31e0e25f1bb355523a63be383ee4594c17c606ec
renekrewinkel <rene@krewinkel.nl>
2024-03-04 08:32:20 +0100

    fixes #10

--- src/main.rs
+++ src/main.rs
@@ -21,29 +21,29 @@ use crate::env::model::Config;
 
 async fn run_completion(prompt: &String, cfg: &Config) -> String {
     if &cfg.use_ollama == "YES" {
-        ollama_completion(prompt, cfg).await
-    } else {
-        openai_completion(prompt, cfg).await
-    }
+        return ollama_completion(prompt, cfg).await;
+    } 
+    
+    openai_completion(prompt, cfg).await
+    
 }
 
 async fn create_search_criteria(input: &String,  cfg: &Config) -> String {
      if cfg.use_ollama == "YES" {
         let search_prompt = format!("create a search argument for the following sentence '{}' no explanation just 1 sentence, omit  bullet points with a maximum of 5 words remove all unnecessary characters", &input);
-        run_completion(&search_prompt, cfg).await
-    } else {
-        // Some weirdness when running openai with return characters (see #9)
-        // work around: just use the input string
-        input.clone()
-    }
+        return run_completion(&search_prompt, cfg).await;
+    } 
+    // Some weirdness when running openai with return characters (see #9)
+    // work around: just use the input string
+    input.clone()
+    
 }
 
 fn extract_model(cfg: &Config) -> String {
     if cfg.use_ollama == "YES" {
-        cfg.ollama_model.clone()
-    } else {
-        cfg.openai_model.clone()
-    }
+        return cfg.ollama_model.clone();
+    } 
+    cfg.openai_model.clone()
 }
 
 #[tokio::main]
